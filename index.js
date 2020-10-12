d43197b7c5f61d8611b196839ab7c143b3dcab7d
Koshoo <benkosh@gmail.com>
2020-10-12 16:36:40 +0300

    s

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const header = document.querySelector('.header');
 const aboutBtn = document.querySelector('#about-btn');
 const navButtons = document.querySelectorAll('.nav-link');
 const goToTopButton = document.querySelector('#toTop');
+const typeWriterText = document.querySelector('.typewriter-text');
+const seeMoreButton = document.querySelector('.see-more');
 
+const toggleButton = document.querySelector('.toggle-button');
+const navlinks = document.querySelectorAll('.nav-links')[0];
 const removeHash = () => {
   history.pushState(
     '',
@@ -14,8 +18,9 @@ const removeHash = () => {
 const handleScroll = (e) => {
   const { scrollY } = e.path[1];
   const header = document.querySelector('.header');
-  if (scrollY === 0) header.style.backgroundColor = '';
+  if (scrollY === 0) header.style.backgroundColor = 'black';
   else {
+    // header.style.backgroundColor = '#ecad0e';
     header.style.backgroundColor = 'black';
   }
 };
@@ -29,6 +34,27 @@ const setSelected = (toSelect) => {
   toSelect.classList.add('selected');
 };
 
+const typewriterOptions = [
+  'Full-Stack Web Developer',
+  'Thinker',
+  'Imaginer',
+  'Team Player'
+];
+
+const loopOverTypeWriter = (options) => {
+  let count = 1;
+  setInterval(() => {
+    typeWriterText.innerHTML = options[count];
+    typeWriterText.style.animation = 'none';
+    typeWriterText.offsetHeight; /* trigger reflow */
+    typeWriterText.style.animation = null;
+    typeWriterText.style.animationDuration = '1s';
+    count++;
+    if (count >= options.length) count = 0;
+  }, 5000);
+};
+// loopOverTypeWriter(typewriterOptions);
+
 window.addEventListener('scroll', handleScroll);
 window.addEventListener('scroll', () => {
   if (window.scrollY > 50) {
@@ -41,6 +67,20 @@ window.addEventListener('hashchange', removeHash);
 window.addEventListener('load', () => window.scrollTo(0, 0));
 
 goToTopButton.addEventListener('click', () => window.scrollTo(0, 0));
+
 navButtons.forEach((navButton) => {
-  navButton.addEventListener('click', () => setSelected(navButton));
+  navButton.addEventListener('click', () => {
+    setSelected(navButton);
+    if (header.classList.contains('active')) {
+      header.classList.toggle('active');
+      navlinks.classList.toggle('active');
+    }
+  });
+});
+seeMoreButton.addEventListener('click', () => setSelected(navButtons[1]));
+
+toggleButton.addEventListener('click', () => {
+  navlinks.classList.toggle('active');
+  header.classList.toggle('active');
+  header.style.backgroundColor = 'black';
 });
